c4c2a511bae29d8cf23aa7e8dc75d21c2cc800f8
unknown <mhdfavascheru@gmail.com>
2024-03-25 07:37:40 +0530

    updated

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,8 +2,9 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers,fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Suspense } from 'react';
 
-export default async function Page({ params }: { params: { id: string } }) {
+export  async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
@@ -28,4 +29,13 @@ export default async function Page({ params }: { params: { id: string } }) {
       <Form invoice={invoice} customers={customers} />
     </main>
   );
+}
+
+
+export default function PageWraper({ params }: { params: { id: string } }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Page params={{id:params.id}} />
+    </Suspense>
+  )
 }
